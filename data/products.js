41953983ccdd18065ebce1877b7df7bc56fd715f
AshutoshMadhesiya <ashutoshmadheshiya03@gmail.com>
2024-08-29 21:51:02 +0530

    use inheritance and polychart

--- data/products.js
+++ data/products.js
@@ -33,9 +33,28 @@ class Product {
     return `$${formatCurrency(this.priceCents)}`;
   }
 
+  extraInfoHTML(){
+    return ``;
+  }
+
   
 }
 
+class clothing extends Product{
+
+  sizeChartLink;
+
+  constructor(productDetails){
+    super(productDetails);
+    this.sizeChartLink=productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML(){
+    return `<a href="${this.sizeChartLink}" target="_blank">
+      Size chart
+    </a>`;
+  }
+}
 
 
 export const products = [
@@ -698,7 +717,9 @@ export const products = [
     ]
   }
 ].map((productDetails)=>{
+
+  if(productDetails.type === 'clothing'){
+    return new clothing(productDetails);
+  }
   return new Product(productDetails);
 });
-
-console.log(products);
